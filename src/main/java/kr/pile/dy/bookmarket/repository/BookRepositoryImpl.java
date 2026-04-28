fc02f574fc90a630c789b31dc56197bfde7e5cf6
songwooyu <songwooyu@gmail.com>
2026-04-28 10:32:05 +0900

    6장 BookMarket add book form 완성

--- src/main/java/kr/pile/dy/bookmarket/repository/BookRepositoryImpl.java
+++ src/main/java/kr/pile/dy/bookmarket/repository/BookRepositoryImpl.java
@@ -108,4 +108,9 @@ public class BookRepositoryImpl implements BookRepository{
 
         return booksByCategory;
     }
+
+    @Override
+    public void setNewBook(Book book) {
+        listOfBooks.add(book);
+    }
 }
--- src/main/java/kr/pile/dy/bookmarket/service/BookServiceImpl.java
+++ src/main/java/kr/pile/dy/bookmarket/service/BookServiceImpl.java
@@ -37,4 +37,9 @@ public class BookServiceImpl implements BookService{
         return booksByFilter;
     }
 
+    @Override
+    public void setNewBook(Book book) {
+        bookRepository.setNewBook(book);
+    }
+
 }
